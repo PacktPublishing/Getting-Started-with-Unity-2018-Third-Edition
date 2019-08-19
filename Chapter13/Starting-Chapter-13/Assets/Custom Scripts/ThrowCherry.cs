2b918c7f2ff6cb972a3b18533070b3eba9bf9aa3
githubce <cegithub@packt.com>
2019-08-19 17:03:59 +0530

    Updated Code Files

---
+++ Chapter13/Starting-Chapter-13/Assets/Custom Scripts/ThrowCherry.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowCherry : MonoBehaviour {
+
+	public CherryControl _CherryControl;
+
+	void Update () {
+
+		//_CherryControl.ThrowACherry();
+	}
+
+}
---
+++ Chapter13/Starting-Chapter-13/Assets/Custom Scripts/VictoryManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VictoryManager : MonoBehaviour {
+
+	Text Victory;
+	int beetleCount;
+	int cucumberCount = 1;	
+	public AudioSource audioSource;
+	public AudioClip victory;
+
+	void Awake () {
+
+		Victory = GetComponent<Text> ();
+		Victory.text = "";
+	}
+
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	} 
+
+	void Update () {
+
+		beetleCount = BeetleManager.currentBeetleCount;
+
+		if (beetleCount == 0) {
+			Victory.text = ("You won!");
+			audioSource.PlayOneShot (victory);
+		}
+
+		cucumberCount = CucumberManager.currentCucumberCount;
+
+		if (cucumberCount == 0) {
+			Victory.text = ("You Lost!");
+		}
+	}
+}
